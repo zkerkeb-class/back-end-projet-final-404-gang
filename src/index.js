c80b5393419466384f50b65a401c90c5bca6f5ef
Mohamed ali Dhouib <94063595+jarlamarel@users.noreply.github.com>
2024-12-09 11:07:55 +0100

    heeeeelp

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const express = require('express');  
-const bodyParser = require('body-parser');  
-const swaggerUi = require('swagger-ui-express');  
-const swaggerDocument = require('./swagger.json');  
+const bodyParser = require('body-parser'); 
 
+require('dotenv').config();
+
+
+console.log('MONGO_URI:', process.env.MONGO_URI);
+//const swaggerUi = require('swagger-ui-express');  
+//const swaggerDocument = require('./swagger.json');  
+
+const connectDB = require('./config/db');
+connectDB();
 const app = express();  
+ 
+const logger = require('./config/logger');
+logger.info('Application started');
+logger.error('An error occurred');
 app.use(bodyParser.json());  
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/test', (req, res) => {
     res.send('Test route is working!');
@@ -13,3 +24,5 @@ app.get('/test', (req, res) => {
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));  
 module.exports = app;  
+//user =dalidhouib08
+//password = h8YOqg9Lk6KQ0f5E
